6a772fb7bdc7168b1bf5abe31aca46eeca95214b
Simple Dimple John <simpledimplejohn@gmail.com>
2021-11-18 07:34:21 -0800

    calculating big O

--- js/BigO.js
+++ js/BigO.js
@@ -1,5 +1,6 @@
 // O(n) means linear 
-
+// O(10000)
+// proportional
 const nemo = "nemo";
 
 const largeArray = new Array(10000).fill('nemo');
@@ -10,6 +11,25 @@ function findNemo(array) {
       console.log(`found nemo number ${array[i]}`)
     }
   }
-
 }
-findNemo(largeArray)
+// findNemo(largeArray)
+
+// CONSTANT TIME:
+// O(1)  only doing one thing
+// O(10) still small
+
+// Big O Math
+
+function funChallenge(input) {
+  let a = 10; // O(1)
+  a = 50 + 3; // O(1)
+  for (let i = 0; i < input.length; i++) { // O(n)
+    anotherFunction();  // O(n)
+    let stranger = true; //O(n)
+    a++; // O(n)
+  }
+  return a; // O(1)
+}
+
+// O(3+4n)
+
