2a83f4c6aa7aef18cbb49f8dc1638c152f3d84bb
Simple Dimple John <simpledimplejohn@gmail.com>
2021-11-18 17:24:46 -0800

    notes for linked list

---
+++ js/LinkedLists.js
@@ -0,0 +1,18 @@
+/*
+head
+nodes: value and pointer
+tail -points to null 
+JAVA has linked lists
+Good at inserting changes and deleting in the middle
+traversal instead of itteration, because you don't know when it will end
+
+prepend O(1) adding to the begining
+append O(1) adding to the end
+lookup O(n) because you have to loop through the list to find anything
+insert O(n)
+delete O(n)
+pointer --> reference to another place in the memory
+Garbage collection
+
+*/
+
---
+++ js/Stacks_Queues.js
@@ -0,0 +1,4 @@
+/*
+Stacks and Queues
+
+*/
