3a73a5d17a2d66d77180b24952de455b0007af1e
Simple Dimple John <simpledimplejohn@gmail.com>
2021-11-18 17:45:03 -0800

    add notes for stacks and queues

--- js/Stacks_Queues.js
+++ js/Stacks_Queues.js
@@ -1,4 +1,21 @@
 /*
 Stacks and Queues
+Linear 
+
+STACKS
+LIFO  like browser history or undo
+- lookup O(n)
+- pop  O(1)   remove last item
+- push O(1)   add an item
+- peek O(1)   look at the top
+
+Queues
+FIFO   like a line to a roller coaster 
+// wait-list app, uber/lift, printer
+- lookup O(n)
+- enqueue O(1)   add to the back of the  queue
+- dequeue O(n)   remove from the front of the queue
+- peek   O(n)    see who's next in line
+
 
 */
